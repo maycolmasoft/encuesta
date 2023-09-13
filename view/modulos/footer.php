a6cd7ee7a0591626742790b9124783891bbbe2d5
Maycol <maycol@masoft.net>
2023-09-13 09:48:50 -0500

    maycol@masoft.net
    Maycol

---
+++ view/modulos/footer.php
@@ -0,0 +1,10 @@
+  
+  
+  
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.0
+    </div>
+    <strong>Copyright &copy; 2020-2021 <a href="www.google.com.ec"> ENCUESTA</a>.</strong> All rights
+    reserved.
+  </footer>
